8b9b63457165ce169044d394bb8e3f0ec4102489
sodastereo <f_er_@hotmail.com>
2020-03-21 01:03:25 -0400

    firebase installed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAa6kuSwd4fgKNp3twz6O4hR2OPJl-jhDQ",
+    authDomain: "clientpanelprod-dfcdc.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-dfcdc.firebaseio.com",
+    projectId: "clientpanelprod-dfcdc",
+    storageBucket: "clientpanelprod-dfcdc.appspot.com",
+    messagingSenderId: "82744343190",
+    appId: "1:82744343190:web:8807328e14feb9dd9620b7",
+    measurementId: "G-8SL9V7N7FR"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAa6kuSwd4fgKNp3twz6O4hR2OPJl-jhDQ",
+    authDomain: "clientpanelprod-dfcdc.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-dfcdc.firebaseio.com",
+    projectId: "clientpanelprod-dfcdc",
+    storageBucket: "clientpanelprod-dfcdc.appspot.com",
+    messagingSenderId: "82744343190",
+    appId: "1:82744343190:web:8807328e14feb9dd9620b7",
+    measurementId: "G-8SL9V7N7FR"
+  }
 };
 
 /*
